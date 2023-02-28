2f5ff6cb79a3a1d56b05fc828a1daa60429d575c
Carlos Ordóñez <carlosordonez@CORDON-MAD-MACP.home>
2023-02-28 14:59:30 +0100

    Put es lang mandatory

--- i18n.ts
+++ i18n.ts
@@ -5,6 +5,8 @@ import enTranslation from './language/en.json';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
+    detection: { order: ['navigator'] },
+    lng: 'es',
     resources: {
         en: { translation: enTranslation },
         es: { translation: esTranslation }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,11 @@ import '@/styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { appWithTranslation } from 'next-i18next';
 import * as React from 'react';
-import i18n from 'i18n';
+import '../../i18n';
+import i18n from '../../i18n';
 
 import type { AppProps } from 'next/app'
-import { I18nextProvider, initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import i18next from 'i18next';
-
-i18next
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'es',
-    debug: true,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+import { I18nextProvider } from 'react-i18next';
 
 function App({ Component, pageProps }: AppProps) {
   return <I18nextProvider i18n={i18n}><Component {...pageProps} /></I18nextProvider>
